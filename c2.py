2f8503c6e0529f225f1248eb6ef988bea3bb2493
MAC <minhyeok@MACui-MacBook-Pro.local>
2017-07-04 07:42:16 +0900

    c2.py

---
+++ c2.py
@@ -0,0 +1,14 @@
+import urllib2
+import string
+
+page = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
+text = page.read().decode("utf8")
+string = ""
+start = text.find('<!--') + 4
+end = text.find('-->')
+
+for c in text[start:end]:
+	if c.isalpha() == True:
+		string += c
+
+print string
